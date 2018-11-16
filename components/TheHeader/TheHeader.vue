06d1ef2696b567849f6099a4b239bb529ffa252c
Majka <mail>
2018-11-16 16:18:14 +0100

    first changes

---
+++ components/TheHeader/TheHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="main-header">
+    <nav class="main-nav">
+      <ul class="nav-links">
+        <nuxt-link 
+          to="/" 
+          tag="li"><a >All Posts </a> </nuxt-link>
+        <nuxt-link
+          to="/about"
+          tag="li"><a > About </a></nuxt-link>
+      </ul>
+    </nav>
+
+  </header>
+</template>
+
+<style scoped>
+.main-header {
+  background-color: #1f1f27;
+  width: 100%;
+  height: 60px;
+  display: flex;
+}
+.nav-links {
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+}
+.nav-links li {
+  min-width: 100px;
+  color: #47494e;
+}
+a {
+  color: #3e3ea5;
+}
+a:active,
+a:hover,
+li .nuxt-link-exact-active a {
+  color: #59b9e4;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,25 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Storyblok and Nuxt',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Awesome blog '
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Oswald:400,700'
+      }
     ]
   },
 
@@ -26,14 +37,12 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: [],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+  plugins: [],
 
   /*
   ** Nuxt.js modules
